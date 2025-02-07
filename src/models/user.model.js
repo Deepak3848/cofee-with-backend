e9cfeec76219a648c1c53b1cbc9b9474ca0c8907
Deepak Marathe <Marathe_d@xoriant.com>
2025-02-07 19:16:47 +0530

    user and vdo model changes added

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  avtar: {
+    type: String, //cloudnery url
+    required: true,
+  },
+  coverImage: {
+    type: String,
+  },
+  watchHistory: [{
+    type: Schema.Types.ObjectId,
+    ref: "Video"
+  }],
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  refreshTocken: {
+    type: String
+  }
+},
+  { timestamps: true }
+)
+
+export const User = mongoose.model('User', userSchema)
